772ddf4245304560dfa69fa04ce6c3519e70fd83
satyannarayanabali <balisatyannarayana@gmail.com>
2017-12-11 16:57:16 +0530

    added new files

---
+++ src/com/ibotrics/fuelfilling/model/FuelFilling1.java
@@ -0,0 +1,125 @@
+package com.ibotrics.fuelfilling.model;
+
+public class FuelFilling1 {
+	private double previousReading;
+	private double presentReading;
+	private double distanceTravelled;
+	private double fuelFilled;
+	private Integer totalFilling;
+	private double totalFuelFilled;
+	private double totalDistanceTravelled;
+	private double mileage;
+	private double avgMileage;
+	/**
+	 * @return the previousReading
+	 */
+	public double getPreviousReading() {
+		return previousReading;
+	}
+	/**
+	 * @param previousReading the previousReading to set
+	 */
+	public void setPreviousReading(double previousReading) {
+		this.previousReading = previousReading;
+	}
+	/**
+	 * @return the presentReading
+	 */
+	public double getPresentReading() {
+		return presentReading;
+	}
+	/**
+	 * @param presentReading the presentReading to set
+	 */
+	public void setPresentReading(double presentReading) {
+		this.presentReading = presentReading;
+	}
+	/**
+	 * @return the distanceTravelled
+	 */
+	public double getDistanceTravelled() {
+		return distanceTravelled;
+	}
+	/**
+	 * @param distanceTravelled the distanceTravelled to set
+	 */
+	public void setDistanceTravelled(double distanceTravelled) {
+		this.distanceTravelled = distanceTravelled;
+	}
+	/**
+	 * @return the fuelFilled
+	 */
+	public double getFuelFilled() {
+		return fuelFilled;
+	}
+	/**
+	 * @param fuelFilled the fuelFilled to set
+	 */
+	public void setFuelFilled(double fuelFilled) {
+		this.fuelFilled = fuelFilled;
+	}
+	/**
+	 * @return the totalFilling
+	 */
+	public Integer getTotalFilling() {
+		return totalFilling;
+	}
+	/**
+	 * @param totalFilling the totalFilling to set
+	 */
+	public void setTotalFilling(Integer totalFilling) {
+		this.totalFilling = totalFilling;
+	}
+	/**
+	 * @return the totalFuelFilled
+	 */
+	public double getTotalFuelFilled() {
+		return totalFuelFilled;
+	}
+	/**
+	 * @param totalFuelFilled the totalFuelFilled to set
+	 */
+	public void setTotalFuelFilled(double totalFuelFilled) {
+		this.totalFuelFilled = totalFuelFilled;
+	}
+	/**
+	 * @return the totalDistanceTravelled
+	 */
+	public double getTotalDistanceTravelled() {
+		return totalDistanceTravelled;
+	}
+	/**
+	 * @param totalDistanceTravelled the totalDistanceTravelled to set
+	 */
+	public void setTotalDistanceTravelled(double totalDistanceTravelled) {
+		this.totalDistanceTravelled = totalDistanceTravelled;
+	}
+	/**
+	 * @return the mileage
+	 */
+	public double getMileage() {
+		return mileage;
+	}
+	/**
+	 * @param mileage the mileage to set
+	 */
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+	/**
+	 * @return the avgMileage
+	 */
+	public double getAvgMileage() {
+		return avgMileage;
+	}
+	/**
+	 * @param avgMileage the avgMileage to set
+	 */
+	public void setAvgMileage(double avgMileage) {
+		this.avgMileage = avgMileage;
+	}
+	
+	
+	
+
+}
